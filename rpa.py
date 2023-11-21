d15400d60f2c1b8e02e58facd9e2e880db8de669
marconatt0 <nicolas.machado@ensino.ulbra.net>
2023-11-21 17:20:01 -0300

    testes para automatizar verificação de 2 fatores

--- rpa.py
+++ rpa.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from PySimpleGUI import PySimpleGUI as sg
+import pyautogui
 
 #criando navegador
 options = webdriver.ChromeOptions()
@@ -15,7 +16,9 @@ nav = webdriver.Chrome(service=service, options=options)
 
 sg.theme('Reddit')
 layout = [
-    [sg.Text('Usuario'), sg.Input(key='usuario')],
+    [sg.Text('Usuario Prime'), sg.Input(key='usuario')],
+    [sg.Text('Senha Prime'), sg.Input(key='senha_prime', password_char='*')],
+    [sg.Text('Email'), sg.Input(key='email')],
     [sg.Text('Senha'), sg.Input(key='senha', password_char='*')],
     [sg.Button('Entrar')]
 ]
@@ -34,15 +37,38 @@ while True:
                         '//*[@id="USUARIO"]').send_keys(valores['usuario'])
 
         nav.find_element('xpath',
-                        '/html/body/table/tbody/tr[2]/td[2]/table/tbody/tr/td[2]/form/table/tbody/tr[4]/td[2]/input').send_keys(valores['senha'])
+                        '/html/body/table/tbody/tr[2]/td[2]/table/tbody/tr/td[2]/form/table/tbody/tr[4]/td[2]/input').send_keys(valores['senha_prime'])
 
         nav.find_element('xpath',
                         '//*[@id="edicao"]/table/tbody/tr[5]/td[2]/table/tbody/tr[1]/td[2]/img').click()
 
-        #Página Inicial
+        nav.execute_script("window.open('https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ifkv=ASKXGp39raIgsOjfrBdzsCVKymIYqifIPB0scbaOya-Sn5QlhZznDQWA4UJ1ehPCNzATzIXyU6H1Ug&rip=1&sacu=1&service=mail&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S-2062608840%3A1700595883599523&theme=glif')")
 
-        def pagina_inicial():
-            nav.get('https://596057.mannesoftprime.com.br/mannesoft/sistema.php?ACAO=publico/caixa_de_trabalho.php&mnsSetRef=1')
+        pyautogui.write(valores['email'])
+                            
+        nav.find_element('xpath', 
+                        '//*[@id="identifierNext"]/div/button/span').click()
+
+        nav.find_element('xpath', 
+                        '/html/body/div[1]/div[1]/div[2]/div/c-wiz/div/div[2]/div/div[1]/div/form/span/section[2]/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input').send_keys(valores['senha'])
+        
+        nav.find_element('xpath', 
+                        '//*[@id="passwordNext"]/div/button/span').click()
+
+        while email == "sim":
+            try:
+                img = pyautogui.locateCenterOnScreen('email_token.PNG', confidence=0.7)
+                pyautogui.click(img.x, img.y)
+                time.sleep(2)
+                email = "não"
+            except:
+                print('não encontrei')
+
+        # login = [valores['usuario'], valores['senha']]
+
+        # with open('login.txt', 'w') as arquivo:
+        #     for valor in login:
+        #         arquivo.write(str(valor) + '\n')
 
         #Aprovação de candidatos
 
@@ -50,15 +76,36 @@ while True:
             nav.execute_script("window.open('https://596057.mannesoftprime.com.br/mannesoft/sistema.php?ACAO=publico/vestibulando.selecao.php&mnsSetRef=1')")
             nav.execute_script("window.open('https://596057.mannesoftprime.com.br/mannesoft/sistema.php?ACAO=publico/dcto.lista.php&dcto=62')")
 
+        #Matrícula de alunos
+
+        def matricula():
+            nav.execute_script("window.open('https://596057.mannesoftprime.com.br/mannesoft/sistema.php?ACAO=publico/aluno.selecao.php&mnsSetRef=1')")
+            nav.execute_script("window.open('https://drive.google.com/file/d/1sxF95U0aEned5wYToZTyeu2NZiPfQKLI/view')")
+
+        #cadastros
+
+        def cadastro_curso():
+            nav.execute_script("window.open('https://596057.mannesoftprime.com.br/mannesoft/sistema.php?ACAO=publico/curso.selecao.php&mnsSetRef=1')")
+            nav.execute_script("window.open('https://596057.mannesoftprime.com.br/mannesoft/sistema.php?ACAO=publico/dcto.lista.php&dcto=53')")
+
+        def cadastro_disciplina():
+            nav.execute_script("window.open('https://596057.mannesoftprime.com.br/mannesoft/sistema.php?ACAO=publico/disc.selecao.php&mnsSetRef=1')")
+            nav.execute_script("window.open('https://596057.mannesoftprime.com.br/mannesoft/sistema.php?ACAO=publico/dcto.lista.php&dcto=54')")
+
+        #Processo seletivo
+        def processo_seletivo():
+            nav.execute_script("window.open('https://596057.mannesoftprime.com.br/mannesoft/sistema.php?ACAO=4&TRANSID=transaction1078119092655cec83dc127')")
+            nav.execute_script("window.open('https://596057.mannesoftprime.com.br/mannesoft/sistema.php?ACAO=publico/dcto.lista.php&dcto=41')")
+
         #menu de opções de telas
 
         sg.theme('Reddit')
         layout = [
-            [sg.Button('Aprovação de alunos')],
-            [sg.Button('...')],
-            [sg.Button('...')],
-            [sg.Button('...')],
-            [sg.Button('...')],
+            [sg.Button('Aprovação de Alunos')],
+            [sg.Button('Matrícula')],
+            [sg.Button('Cadastro Curso')],
+            [sg.Button('Cadastro Disciplina')],
+            [sg.Button('Processo Seletivo')],
             [sg.Button('Sair')]
         ]
 
@@ -68,6 +115,13 @@ while True:
             eventos, valores = janela.read()
             if eventos == sg.WIN_CLOSED or eventos == 'Sair':
                 break
-            elif eventos == 'Aprovação de alunos':
+            elif eventos == 'Aprovação de Alunos':
                 aprovacao_aluno()
-            
+            elif eventos == 'Matrícula':
+                matricula()
+            elif eventos == 'Cadastro Curso':
+                cadastro_curso()
+            elif eventos == 'Cadastro Disciplina':
+                cadastro_disciplina()
+            elif eventos == 'Processo Seletivo':
+                processo_seletivo()
